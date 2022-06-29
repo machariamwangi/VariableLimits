fcc4581ae1c7668e494be79e714ed38de2d84e2a
ammwangi <elijah.mwangi@kamsware.com>
2022-06-29 09:28:49 +0300

    fun with arrays

---
+++ FunWithStrings/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace FunWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "Hello World";
+            string s2 = s1.Substring(1, 2);
+            Console.WriteLine($"s1: {s1}, s2: {s2}");
+
+            string[] strings = { "Abc", "cde", "efg", "ghi", "ijk", "lmn" };
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.WriteLine($"Row {i + 1}: {strings[i]}");
+            }
+
+            for (int i = strings.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"Row {i + 1}: {strings[i]}");
+            }
+
+            string[,] solid = {
+                                    { "*", "*", "*",  "*", "*"},
+                                    { "*", "*", "*",  "*", "*"},
+                                    { "*", "*", "*",  "*", "*"},
+                                    { "*", "*", "*",  "*", "*"},
+                                    { "*", "*", "*",  "*", "*"}
+                              };
+            string[,] thex = {
+                                    { "*", "", "",  "", "*"},
+                                    { "", "*", "",  "*", ""},
+                                    { "", "", "*",  "", ""},
+                                    { "", "*", "",  "*", ""},
+                                    { "*", "", "",  "", "*"}
+                              };
+
+            for (var i=0; i< solid.GetLength(0); i++)
+            {
+                for (var j=0; j<solid.GetLength(1); j++)
+                {
+                    Console.Write(solid[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            for (var i = 0; i < thex.GetLength(0); i++)
+            {
+                for (var j = 0; j < thex.GetLength(1); j++)
+                {
+                    Console.Write(thex[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            string[,] result = new string[5, 5];
+            for (var i = 0; i < thex.GetLength(0); i++)
+            {
+                for (var j = 0; j < thex.GetLength(1); j++)
+                {
+                    if (solid[i, j] != thex[i, j]) result[i, j] = "*";
+                    else result[i, j] = "";
+                }
+                Console.WriteLine();
+            }
+
+            for (var i = 0; i < result.GetLength(0); i++)
+            {
+                for (var j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SnakeSolution/Program.cs
@@ -0,0 +1,324 @@
+﻿using System;
+
+namespace SnakeSolution
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string strInput = "A B C D E F G H I J K L M N O P Q R S T U V W X Y";
+
+            string[] strArray = strInput.Split(' ');
+
+            int nDim = (int)Math.Sqrt(strArray.GetLength(0));
+
+
+
+            string[,] arrCh;
+
+            arrCh = new string[nDim, nDim];
+
+
+
+            int r = 0, c = 0;//row,col
+
+            int r1 = 0, c1 = 0; //start
+
+            int r2 = nDim - 1, c2 = nDim - 1;//end
+
+
+
+            int i = 0, nLen = nDim * nDim;
+
+
+
+            //fill array
+
+            char chDir = 'r'; //right
+
+            do
+
+            {
+
+                arrCh[r, c] = strArray[i];
+
+                //Console.WriteLine($"[{r},{c}]={strLetters[i]}");
+
+                i++;
+
+                switch (chDir)
+
+                {
+
+                    case 'r':
+
+                        if (c == c2)
+
+                        {
+
+                            chDir = 'd'; //down
+
+                            r1++;
+
+                            r++;
+
+                        }
+
+                        else
+
+                        {
+
+                            c++;
+
+                        }
+
+                        break;
+
+                    case 'd':
+
+                        if (r == r2)
+
+                        {
+
+                            chDir = 'l';//left
+
+                            c2--;
+
+                            c--;
+
+                        }
+
+                        else
+
+                        {
+
+                            r++;
+
+                        }
+
+                        break;
+
+                    case 'l':
+
+                        if (c == c1)
+
+                        {
+
+                            chDir = 'u';//up
+
+                            r2--;
+
+                            r--;
+
+                        }
+
+                        else
+
+                        {
+
+                            c--;
+
+                        }
+
+                        break;
+
+                    case 'u':
+
+                        if (r == r1)
+
+                        {
+
+                            chDir = 'r';//right
+
+                            c1++;
+
+                            c++;
+
+                        }
+
+                        else
+
+                        {
+
+                            r--;
+
+                        }
+
+                        break;
+
+                    default:
+
+                        break;
+
+                }
+
+
+
+            } while (i < nLen);
+
+
+
+            //show array
+
+            for (int o1 = 0; o1 < nDim; o1++)
+
+            {
+
+                for (int o2 = 0; o2 < nDim; o2++)
+
+                {
+
+                    Console.Write($"{arrCh[o1, o2]} ");
+
+                }
+
+                Console.Write("\r\n");
+
+            }
+
+
+
+            Console.WriteLine();
+
+            //final output
+
+            r = 0;
+
+            c = 0;//row,col
+
+            r1 = 0;
+
+            c1 = 0; //start
+
+            r2 = nDim - 1;
+
+            c2 = nDim - 1;//end
+
+            i = 0;
+
+            chDir = 'r'; //right
+
+            do
+
+            {
+
+                Console.Write($"{arrCh[r, c]} ");
+
+                i++;
+
+                switch (chDir)
+
+                {
+
+                    case 'r':
+
+                        if (c == c2)
+
+                        {
+
+                            chDir = 'd'; //down
+
+                            r1++;
+
+                            r++;
+
+                        }
+
+                        else
+
+                        {
+
+                            c++;
+
+                        }
+
+                        break;
+
+                    case 'd':
+
+                        if (r == r2)
+
+                        {
+
+                            chDir = 'l';//left
+
+                            c2--;
+
+                            c--;
+
+                        }
+
+                        else
+
+                        {
+
+                            r++;
+
+                        }
+
+                        break;
+
+                    case 'l':
+
+                        if (c == c1)
+
+                        {
+
+                            chDir = 'u';//up
+
+                            r2--;
+
+                            r--;
+
+                        }
+
+                        else
+
+                        {
+
+                            c--;
+
+                        }
+
+                        break;
+
+                    case 'u':
+
+                        if (r == r1)
+
+                        {
+
+                            chDir = 'r';//right
+
+                            c1++;
+
+                            c++;
+
+                        }
+
+                        else
+
+                        {
+
+                            r--;
+
+                        }
+
+                        break;
+
+                    default:
+
+                        break;
+
+                }
+
+            } while (i < nLen);
+
+        }
+
+    }
+}
