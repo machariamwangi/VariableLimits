663c43efc99a724d1f4159a6dffb9f6009637754
ammwangi <elijah.mwangi@kamsware.com>
2022-06-29 12:41:08 +0300

    foreach and switch case logics

---
+++ MoreLogicFlows/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoreLogicFlows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var value in array)
+            {
+                switch (value)
+                {
+                    case 1:
+                        Console.WriteLine($"Found 1");
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine($"Found 2, 3 or 4 {value}");
+                        break;
+                    default:
+                        Console.WriteLine($"Found something else {value}");
+                        break;
+                }
+            }
+        }
+    }
+}
--- SnakeSolution/Program.cs
+++ SnakeSolution/Program.cs
@@ -3,322 +3,101 @@
 namespace SnakeSolution
 {
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
+            int matrixSize = 5;
+            string sentence = @"First we thought the PC was a calculator. Then we found out how to turn numbers into letters with ASCII and we thought it was a typewriter. Then we discovered graphics, and we thought it was a television. With the World Wide Web, we've realized it's a brochure.";  // Quote from Douglas Adams
 
-            string strInput = "A B C D E F G H I J K L M N O P Q R S T U V W X Y";
+            string[] wordList = sentence.Split(" ");
+            string[,] matrix = new string[matrixSize, matrixSize];
+            bool readInMode = true;
 
-            string[] strArray = strInput.Split(' ');
-
-            int nDim = (int)Math.Sqrt(strArray.GetLength(0));
-
-
-
-            string[,] arrCh;
-
-            arrCh = new string[nDim, nDim];
-
-
-
-            int r = 0, c = 0;//row,col
-
-            int r1 = 0, c1 = 0; //start
-
-            int r2 = nDim - 1, c2 = nDim - 1;//end
-
-
-
-            int i = 0, nLen = nDim * nDim;
-
-
-
-            //fill array
-
-            char chDir = 'r'; //right
-
-            do
-
+            // This first loop goes once to start the words and the second time to get the words and write them out.
+            for (var j = 0; j < 2; j++)
             {
+                // The adders determine whether rows and columns are increasing, decreasing, or staying the same.
+                int rowAdder = 0;
+                int columnAdder = 1;
 
-                arrCh[r, c] = strArray[i];
+                // These are the indexes into the matrix to put words in or to take them out.
+                int currentRow = 0;
+                int currentCol = 0;
 
-                //Console.WriteLine($"[{r},{c}]={strLetters[i]}");
+                // For a 5x5 matrix, we need to handle 5 columns, then 4 rows, then 4 columns etc. This handles those changes.
+                int countdownStart = 4;
+                int countdown = 4;
+                int countdownSub = 1;
 
-                i++;
-
-                switch (chDir)
-
+                for (var i = 0; i < 25; i++)
                 {
+                    string word;
 
-                    case 'r':
+                    if (readInMode)
+                    {
+                        word = wordList[i];
+                        matrix[currentRow, currentCol] = word;
+                    }
+                    if (!readInMode)
+                    {
+                        Console.Write($"{matrix[currentRow, currentCol]} ");
+                    }
 
-                        if (c == c2)
-
+                    // If we've reached the countdown, change directions.
+                    if (countdown == 0)
+                    {
+                        if (rowAdder == 0 && columnAdder == 1)
                         {
-
-                            chDir = 'd'; //down
-
-                            r1++;
-
-                            r++;
-
+                            rowAdder = 1;
+                            columnAdder = 0;
+                        }
+                        else if (rowAdder == 1 && columnAdder == 0)
+                        {
+                            rowAdder = 0;
+                            columnAdder = -1;
+                        }
+                        else if (rowAdder == 0 && columnAdder == -1)
+                        {
+                            rowAdder = -1;
+                            columnAdder = 0;
+                        }
+                        else if (rowAdder == -1 && columnAdder == 0)
+                        {
+                            rowAdder = 0;
+                            columnAdder = 1;
                         }
 
-                        else
+                        countdownStart -= countdownSub;
+                        countdown = countdownStart;
+                        if (countdownSub == 0) countdownSub = 1;
+                        else countdownSub = 0;
+                    }
+                    else
+                    {
+                        countdown--;
+                    }
 
-                        {
-
-                            c++;
-
-                        }
-
-                        break;
-
-                    case 'd':
-
-                        if (r == r2)
-
-                        {
-
-                            chDir = 'l';//left
-
-                            c2--;
-
-                            c--;
-
-                        }
-
-                        else
-
-                        {
-
-                            r++;
-
-                        }
-
-                        break;
-
-                    case 'l':
-
-                        if (c == c1)
-
-                        {
-
-                            chDir = 'u';//up
-
-                            r2--;
-
-                            r--;
-
-                        }
-
-                        else
-
-                        {
-
-                            c--;
-
-                        }
-
-                        break;
-
-                    case 'u':
-
-                        if (r == r1)
-
-                        {
-
-                            chDir = 'r';//right
-
-                            c1++;
-
-                            c++;
-
-                        }
-
-                        else
-
-                        {
-
-                            r--;
-
-                        }
-
-                        break;
-
-                    default:
-
-                        break;
+                    // Update the indices in the proper direction (right, then down, then left, then up, etc.
+                    currentRow += rowAdder;
+                    currentCol += columnAdder;
 
                 }
 
-
-
-            } while (i < nLen);
-
-
-
-            //show array
-
-            for (int o1 = 0; o1 < nDim; o1++)
-
-            {
-
-                for (int o2 = 0; o2 < nDim; o2++)
-
+                if (readInMode)
                 {
-
-                    Console.Write($"{arrCh[o1, o2]} ");
-
+                    // Output the matrix
+                    for (var row = 0; row < matrixSize; row++)
+                    {
+                        for (var col = 0; col < matrixSize; col++)
+                        {
+                            Console.Write($"{matrix[row, col]} ");
+                        }
+                        Console.WriteLine();
+                    }
                 }
-
-                Console.Write("\r\n");
-
+                readInMode = false;
             }
 
-
-
-            Console.WriteLine();
-
-            //final output
-
-            r = 0;
-
-            c = 0;//row,col
-
-            r1 = 0;
-
-            c1 = 0; //start
-
-            r2 = nDim - 1;
-
-            c2 = nDim - 1;//end
-
-            i = 0;
-
-            chDir = 'r'; //right
-
-            do
-
-            {
-
-                Console.Write($"{arrCh[r, c]} ");
-
-                i++;
-
-                switch (chDir)
-
-                {
-
-                    case 'r':
-
-                        if (c == c2)
-
-                        {
-
-                            chDir = 'd'; //down
-
-                            r1++;
-
-                            r++;
-
-                        }
-
-                        else
-
-                        {
-
-                            c++;
-
-                        }
-
-                        break;
-
-                    case 'd':
-
-                        if (r == r2)
-
-                        {
-
-                            chDir = 'l';//left
-
-                            c2--;
-
-                            c--;
-
-                        }
-
-                        else
-
-                        {
-
-                            r++;
-
-                        }
-
-                        break;
-
-                    case 'l':
-
-                        if (c == c1)
-
-                        {
-
-                            chDir = 'u';//up
-
-                            r2--;
-
-                            r--;
-
-                        }
-
-                        else
-
-                        {
-
-                            c--;
-
-                        }
-
-                        break;
-
-                    case 'u':
-
-                        if (r == r1)
-
-                        {
-
-                            chDir = 'r';//right
-
-                            c1++;
-
-                            c++;
-
-                        }
-
-                        else
-
-                        {
-
-                            r--;
-
-                        }
-
-                        break;
-
-                    default:
-
-                        break;
-
-                }
-
-            } while (i < nLen);
-
         }
-
     }
 }
